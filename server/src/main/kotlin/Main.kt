a2a6c270c39ed29beba67b672aa89f5c4c831455
Neo Ighodaro <neo@creativitykills.co>
2017-12-05 11:48:52 +0100

    Update Main.kt

--- server/src/main/kotlin/Main.kt
+++ server/src/main/kotlin/Main.kt
@@ -8,18 +8,20 @@ import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
 import java.util.*
 
+
 fun Application.main() {
+
+    val pusher = Pusher("PUSHER_APP_ID", "PUSHER_APP_KEY", "PUSHER_APP_SECRET")
+    pusher.setCluster("PUSHER_APP_CLUSTER")
+
     install(DefaultHeaders)
     install(CallLogging)
     install(Routing) {
         get("/{message}") {
             val i = call.parameters["message"]!!
-            val pusher = Pusher("PUSHER_ID", "PUSHER_KEY", "PUSHER_SECRET")
-            pusher.setCluster("eu")
             pusher.trigger("my-channel", "my-event", Collections.singletonMap("message", i))
             call.respond("response sent")
         }
 
     }
-
 }
